8592c0d9e7e08871888e244188dc7ac756a6da5a
Bill Baker <bill@orangecrayon.com>
2019-10-07 12:12:09 -0400

    default to Bahiyyih's quilt

--- src/State.ts
+++ src/State.ts
@@ -12,10 +12,12 @@ export interface AppState {
 
 export const DEFAULT_APP_STATE: AppState = {
     pickers: Map({"web": false, "default": true, "sketch": false, "chrome": false}),
-    colors: List([ "#dfdb3b", "#1c506c" ]),
-    quiltSize: new Point(5, 4),
+    colors: List([
+       "#fff0a3", "#fff0a3", "#fff0a3", "#fae783", "#f7df5e", "#f7df5e", "#f7df5e", "#f7df5e", "#f7d65e", "#f7d65e", "#ffc11f", "#ffc11f", "#ffa91f", "#ffa91f", "#6f71c0", "#3b3edf", "#3b3edf", "#185b21", "#226e19", "#66ac5d",
+    ]),
+    quiltSize: new Point(4, 5),
     showBorder: true,
-    borderColor: "#1c506c",
+    borderColor: "#1b3272",
     errors: List(), //["error", "another, longer error"])
 }
 
